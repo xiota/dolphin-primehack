95c7a75850f530c5616151f0538134b4a91a4b98
xiota <github@mentalfossa.com>
2024-10-28 21:18:20 +0000

    feat(primehack): Core/*

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -9,7 +9,7 @@
 
 namespace Common
 {
-#define EMULATOR_NAME "Dolphin"
+#define EMULATOR_NAME "PrimeHack"
 
 #ifdef _DEBUG
 #define BUILD_TYPE_STR "Debug "
--- Source/Core/DolphinNoGUI/MainNoGUI.cpp
+++ Source/Core/DolphinNoGUI/MainNoGUI.cpp
@@ -132,6 +132,10 @@ void Host_YieldToUI()
 {
 }
 
+void Host_RendererUpdateCursor(bool locked) {
+  s_platform->UpdateCursor(locked);
+}
+
 void Host_TitleChanged()
 {
 #ifdef USE_DISCORD_PRESENCE
