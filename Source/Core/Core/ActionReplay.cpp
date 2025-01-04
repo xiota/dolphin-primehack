1ae75d3098e2b3e1eefd380c07ac65e6875cb88c
xiota <github@mentalfossa.com>
2025-01-04 22:09:07 +0000

    feat(primehack): Core/Core

--- Source/Core/Core/ActionReplay.cpp
+++ Source/Core/Core/ActionReplay.cpp
@@ -44,6 +44,8 @@
 #include "Core/Config/MainSettings.h"
 #include "Core/PowerPC/MMU.h"
 
+#include "PrimeHack/HackConfig.h"
+
 namespace ActionReplay
 {
 enum
@@ -995,6 +997,8 @@ static bool RunCodeLocked(const Core::CPUThreadGuard& guard, const ARCode& arcod
 
 void RunAllActive(const Core::CPUThreadGuard& cpu_guard)
 {
+  prime::GetHackManager()->run_active_mods(cpu_guard);
+
   if (!Config::AreCheatsEnabled())
     return;
 
