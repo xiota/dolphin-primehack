xiota/dolphin

feat(primehack): Core/DolphinQt

Source/Core/DolphinQt/Settings/GeneralPane.h
@@ -42,6 +42,7 @@ private:
   QComboBox* m_combobox_fallback_region;
   QCheckBox* m_checkbox_dualcore;
   QCheckBox* m_checkbox_cheats;
+  QCheckBox* m_checkbox_primehack;
   QCheckBox* m_checkbox_override_region_settings;
   QCheckBox* m_checkbox_auto_disc_change;
 #ifdef USE_DISCORD_PRESENCE

xiota/dolphin-primehack

Source/Core/DolphinQt/Settings/GeneralPane.h
@@ -47,6 +47,7 @@ private:
   ToolTipComboBox* m_combobox_fallback_region;
   ConfigBool* m_checkbox_dualcore;
   ConfigBool* m_checkbox_cheats;
+  ConfigBool* m_checkbox_primehack;
   ConfigBool* m_checkbox_override_region_settings;
   ConfigBool* m_checkbox_auto_disc_change;
 #ifdef USE_DISCORD_PRESENCE