0301bccd2b5a3cbf2852c2d653444535c45c797a
xiota <github@mentalfossa.com>
2025-03-13 02:11:15 +0000

    feat(primehack): Core/Core/HW

--- Source/Core/Core/HW/WiimoteEmu/Extension/Nunchuk.h
+++ Source/Core/Core/HW/WiimoteEmu/Extension/Nunchuk.h
@@ -158,6 +158,8 @@ public:
   void Reset() override;
   void DoState(PointerWrap& p) override;
 
+  void ChangeUIPrimeHack(bool useMetroidUI);
+
   ControllerEmu::ControlGroup* GetGroup(NunchukGroup group);
 
   void LoadDefaults() override;
@@ -182,6 +184,8 @@ public:
   static constexpr const char* Z_BUTTON = "Z";
 
 private:
+  bool using_metroid_ui = false;
+
   ControllerEmu::Tilt* m_tilt;
   ControllerEmu::Force* m_swing;
   ControllerEmu::Shake* m_shake;
