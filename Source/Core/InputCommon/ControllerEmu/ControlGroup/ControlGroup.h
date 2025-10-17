a355d05118afcaa3855f00b2da2b365d1e7777c7
xiota <github@mentalfossa.com>
2025-10-17 03:17:04 +0000

    feat(primehack): Core/InputCommon

--- Source/Core/InputCommon/ControllerEmu/ControlGroup/ControlGroup.h
+++ Source/Core/InputCommon/ControllerEmu/ControlGroup/ControlGroup.h
@@ -51,6 +51,8 @@ enum class GroupType
   IMUGyroscope,
   IMUCursor,
   IRPassthrough,
+  PrimeHackMode,
+  PrimeHackAltProfile
 };
 
 class ControlGroup
@@ -109,6 +111,8 @@ public:
   const DefaultValue default_value;
   SettingValue<bool> enabled;
   std::unique_ptr<NumericSetting<bool>> enabled_setting;
+
+  bool use_metroid_ui = false;
 
   std::vector<std::unique_ptr<Control>> controls;
   std::vector<std::unique_ptr<NumericSettingBase>> numeric_settings;
